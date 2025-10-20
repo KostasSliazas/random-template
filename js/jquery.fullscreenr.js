57ea07a4fc65c00798695f3a665553bbdbf83dc0
Kostas <kostas.sliazas@gmail.com>
2025-10-20 19:20:29 +0300

    Add files via upload

---
+++ js/jquery.fullscreenr.js
@@ -0,0 +1,31 @@
+(function($){	
+	$.fn.fullscreenr = function(options) {
+		if(options.height === undefined) alert('Nustatymai nepakeisti.');
+		if(options.width === undefined) alert('Nustatymai nepakeisti.');
+		if(options.bgID === undefined) alert('Reikia paveikslo ID, kaip numatytas bus naudojamas #bgimg.');
+		var defaults = { width: 1280,  height: 1024, bgID: 'bgimg' };
+		var options = $.extend({}, defaults, options); 
+		$(document).ready(function() { $(options.bgID).fullscreenrResizer(options);	});
+		$(window).bind("resize", function() { $(options.bgID).fullscreenrResizer(options); });		
+		return this; 		
+	};	
+	$.fn.fullscreenrResizer = function(options) {
+		// Set bg size
+		var ratio = options.height / options.width;	
+		// Get browser window size
+		var browserwidth = $(window).width();
+		var browserheight = $(window).height();
+		// Scale the image
+		if ((browserheight/browserwidth) > ratio){
+		    $(this).height(browserheight);
+		    $(this).width(browserheight / ratio);
+		} else {
+		    $(this).width(browserwidth);
+		    $(this).height(browserwidth * ratio);
+		}
+		// Center the image
+		$(this).css('left', (browserwidth - $(this).width())/2);
+		$(this).css('top', '0');
+		return this; 		
+	};
+})(jQuery);
